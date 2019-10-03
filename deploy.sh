5b3358a869594d6f8c682b258ddb2e2f775be26d
Rocco Georgi <rocco@pavingways.com>
2019-10-04 00:49:03 +0200

    site update 2019-10-04 00:49:03

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-rm -rf _deploy/*
-cp -a _site/* _deploy/
-cd _deploy/
-mv index.html new.html
-git add -A
-git commit -m "site update on `date +'%Y-%m-%d %H:%M:%S'`"
-git push -f origin master
-cd ..
